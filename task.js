ef0b1aea1540bf8f1dd270c1c51719704b017b47
Your Name~ <ryanhuseman@gmail.com>
2024-03-16 21:47:57 -0500

    second commit

--- task.js
+++ task.js
@@ -21,23 +21,22 @@
 // This wasn't fittiing the requirements of the assignment. 
 // });
 
-
-
-document.getElementById('newTask').addEventListener('click', (event) =>{
+document.getElementById('newTask').addEventListener('click', (event) =>{// adding an event listener for a mouse click 
+    //for the button with id newTask
     
-    event.preventDefault();
+    event.preventDefault(); // prevents from submitting and the page from refreshing
     let table = document.getElementById('tasks');
-    let insertTasks = document.getElementById("insertTask");
-    let input = taskForm.querySelector("input");
+    let insertTasks = document.getElementById("insertTask");// assigning inserTasks to the div inserTask
+    let input = taskForm.querySelector("input");//Selecting the input tag in the form
     
-    if(input.value.trim() ===""){
+    if(input.value.trim() ===""){ // checking for input and if none then "Please enter task"
         insertTasks.textContent = "Please enter task!";
-    } else {
+    } else {// if they did enter text in the input
 
-let row = table.insertRow(1);
-row.insertCell(0).innerHTML = document.getElementById("taskInput").value;
+let row = table.insertRow(1);// variable row = inserting a new row
+row.insertCell(0).innerHTML = document.getElementById("taskInput").value;// assinging the data to the first cell in the row
 
-document.getElementById('taskInput').value = "";
+document.getElementById('taskInput').value = ""; // Value of taskInput which is the task inserted
     }
 });
 
